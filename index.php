cf1e14026166bf873b24702682aa16c6ef3810e8
PutYourNameOnIt <caromalinis@gmail.com>
2025-06-11 21:13:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if ($res && $row = sqlsrv_fetch_array($res, SQLSRV_FETCH_ASSOC)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dhen's Kitchen - Home</title>
+    <title>Home - Dhen's Kitchen</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
